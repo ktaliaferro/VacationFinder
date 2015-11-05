cc35a0fc71ac62b2cacde8c4bbaf3de9e818c0d4
Kenny Taliaferro <kennethtaliaferro@gmail.com>
2015-11-05 12:05:42 -0600

    cleaned up comments

--- webapp/app/get_rankings.py
+++ webapp/app/get_rankings.py
@@ -1,19 +1,19 @@
 """This function takes users interests as inputs and outputs a list of
 ranked vacation destinations with top interests for each destination.
-To rank destinations, the app uses data that I've collected from Twitter
+To rank destinations, the app uses data that has been collected from Twitter
 and stored in MySQL.  user_interests is a assumed to be a string
-consiting of interests separated by commas.  The output is a 2D list of
+consisting of interests separated by commas.  The output is a 2D list of
 strings.  The left column has destinations and the right column has
 the top three interests for that destination."""
 
-# packages
+# Packages
 import re
 import time
 import pandas as pd
 import pymysql as mdb
 from sqlalchemy import create_engine
 
-# files in this directory
+# Files in this directory
 from mysql_login_info import sql_username, sql_password
 import preprocessing
 
@@ -24,10 +24,10 @@ def get_rankings(user_interests):
     df_destinations=pd.read_csv('vacation_destinations.txt', header=None, names=['Destination'])
     destinations = sorted(df_destinations['Destination'].values.tolist())
 
-    # connect to my local mySQL server and open the vacation database
+    # Connect to the local mySQL server and open the vacation database.
     engine = create_engine('mysql+mysqldb://'+sql_username+':'+sql_password+'@127.0.0.1:3306/vacation', echo=False)
 
-    # Parse user input, stem it, and make regexes
+    # Parse user input, stem it, and make regexes.
     user_input = user_interests
     def stem(s):
             return preprocessing.preprocess_pipeline(s, return_as_str=True, do_remove_stopwords=True, do_clean_html=False)
@@ -36,11 +36,11 @@ def get_rankings(user_interests):
     for interest in user_input.split(',')[:max_interests]:
         if interest == '':
             continue
-        # remove any characters that that aren't alphanumeric.
+        # Remove any characters that that aren't alphanumeric.
         interest = re.sub(r'[^\w]+', '', interest)
         if False:
-            # Insert MySQL regex for spaces (this isn't working yet).  Once I
-            # get this working, make sure to fix the re.sub line above
+            # Insert MySQL regex for spaces (this isn't working yet).  Once
+            # this is working, make sure to edit the re.sub line above
             # so that it doesn't remove spaces.
             interests_regex[interest.strip()]= re.sub(' ',r'[[:space:]]',stem(interest))
         interests_regex[interest.strip()]= stem(interest)
@@ -48,11 +48,11 @@ def get_rankings(user_interests):
     print len(interests), 'interests'
     print interests_regex
 
-    # Put the destinations in a dataframe to use in the next step
+    # Put the destinations in a dataframe to use in the next step.
     df_destinations = pd.DataFrame({'Destination':destinations}).set_index('Destination')
     df_destinations.head()
        
-    # Count the number of tweets for each destination for each interest
+    # Count the number of tweets for each destination for each interest.
     destinations_interests = df_destinations.copy()
     db = mdb.connect(user=sql_username, host="localhost", passwd=sql_password, db="vacation", charset='utf8')
     interests_processed = 0
@@ -74,11 +74,10 @@ def get_rankings(user_interests):
         interests_processed += 1
         print interests_processed, 'interests processed'
         
-    # If you want to normalize the rows,
-    # change normalize_rows to True.
+    # To normalize rows, change normalize_rows to True.
     normalize_rows = False
     if normalize_rows:
-        # count the number of timeline tweets for each destination
+        # Count the number of timeline tweets for each destination.
         db = mdb.connect(user=sql_username, host="localhost", passwd=sql_password, db="vacation", charset='utf8')
         sql_query = ("SELECT Destination, COUNT(*)" +
                      " FROM timeline_tweets_stemmed_unique GROUP BY Destination")
@@ -87,16 +86,19 @@ def get_rankings(user_interests):
             cur.execute(sql_query)
             query_results = cur.fetchall()
         query_results = map(list, zip(*[list(i) for i in query_results]))
-        df_tweets_per_destination = pd.DataFrame({'Destination': query_results[0], 'Tweets': query_results[1]}).set_index('Destination')
+        df_tweets_per_destination = pd.DataFrame({'Destination': query_results[0],
+                'Tweets': query_results[1]}).set_index('Destination')
         destinations_interests_rows_normalized = destinations_interests.join(df_tweets_per_destination, how='outer').fillna(0)
 
         for interest in interests:
-            destinations_interests_rows_normalized[interest]=destinations_interests_rows_normalized[interest].astype(float) / destinations_interests_rows_normalized['Tweets'].astype(float)
+            destinations_interests_rows_normalized[interest] = (
+                    destinations_interests_rows_normalized[interest].astype(float) /
+                    destinations_interests_rows_normalized['Tweets'].astype(float))
         destinations_interests_rows_normalized = destinations_interests_rows_normalized.drop('Tweets', axis=1)
     else:
         destinations_interests_rows_normalized = destinations_interests
 
-    # Normalize each interest column
+    # Normalize each interest column.
     def normalize(x):
             if x.std() > 0.01:
                 return (x-x.mean())/x.std()
@@ -106,26 +108,26 @@ def get_rankings(user_interests):
     destinations_interests_scores = destinations_interests_rows_normalized.apply(normalize)
 
     # Rank the destinations and then
-    # store the top three interests for each destination in a dataframe
+    # store the top three interests for each destination in a dataframe.
     destination_col = destinations_interests_scores.sum(axis=1).sort(ascending=False, inplace=False).index.values.tolist()
     interest_col = []
     for destination in destination_col:
         interest_list=[]
-        # rank the interests
+        # Rank the interests.
         for interest in destinations_interests_scores.loc[destination].sort(ascending=False, inplace=False).index.values.tolist():
-            # only include interests with associated tweets
+            # Only include interests with associated tweets.
             if destinations_interests.loc[[destination]][interest][0]>0:
                 interest_list.append(interest)
-        # only use the top 3 interests
+        # Only use the top 3 interests.
         interest_col.append(', '.join(interest_list[:3]))  
     df_interest_destination = pd.DataFrame({'Interest': interest_col, 'Destination': destination_col})
-    # reorder the columns
+    # Reorder the columns.
     df_interest_destination = df_interest_destination[['Destination', 'Interest']]
-    # drop destinations without any interests
+    # Drop destinations without any interests.
     df_interest_destination=df_interest_destination[df_interest_destination['Interest'] != '']
     df_interest_destination.head()
     
-    # Put the top interest for each destination in table format
+    # Put the top interest for each destination in a list format.
     interest_col = df_interest_destination['Interest'].values.tolist()
     destination_col = df_interest_destination['Destination'].values.tolist()
     interest_destination = []
@@ -133,6 +135,6 @@ def get_rankings(user_interests):
     for i in range(length):
         interest_destination.append([destination_col[i],interest_col[i]])
 
-    # Return the results as a 2D list
+    # Return the results as a 2D list.
     return interest_destination
 
