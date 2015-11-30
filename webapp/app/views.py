594c802d7d250aece3919028f31863b54e1bfc75
Kenny Taliaferro <ktaliaferro@users.noreply.github.com>
2015-11-30 17:06:05 -0600

    cleaned up indentation in views.py

--- webapp/app/views.py
+++ webapp/app/views.py
@@ -16,24 +16,24 @@ app = Flask(__name__)
 @app.route('/index')    
 @app.route('/input')
 def cities_input():
-  return render_template("input.html")
+    return render_template("input.html")
 
 @app.route('/output')
 def cities_output():
-  interests = request.args.get('ID') # pull interests from the input page
-  start = time.time()
-  # If the user inputs the string 'cached_results', then the app will return 
-  # cached results for boats, surfing, kayaking, and swimming.  This is for 
-  # testing and demonstration purposes only.
-  if interests == 'cached_results':
-    interests = 'boats, surfing, kayaking, swimming'
-    rankings = get_rankings_dummy.get_rankings_dummy()
-  else:
-    rankings = get_rankings.get_rankings(interests)
-  end = time.time()
-  print 'runtime', end-start, 'seconds'
-  length = min(20,len(rankings))
-  return render_template("output.html", rankings = rankings, interests=interests, length=length, runtime=end-start)
+    interests = request.args.get('ID') # pull interests from the input page
+    start = time.time()
+    # If the user inputs the string 'cached_results', then the app will return 
+    # cached results for boats, surfing, kayaking, and swimming.  This is for 
+    # testing and demonstration purposes only.
+    if interests == 'cached_results':
+        interests = 'boats, surfing, kayaking, swimming'
+        rankings = get_rankings_dummy.get_rankings_dummy()
+    else:
+        rankings = get_rankings.get_rankings(interests)
+    end = time.time()
+    print 'runtime', end-start, 'seconds'
+    length = min(20,len(rankings))
+    return render_template("output.html", rankings = rankings, interests=interests, length=length, runtime=end-start)
   
 @app.route('/slides')
 def slides():
